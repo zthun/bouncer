89567e7f8567d4fe8b6dcd97e0a283798e82712d
Anthony Bonta <zthun.coding@gmail.com>
2024-11-17 17:30:46 -0600

    chore: local config file

--- .config/bouncerrc.ts
+++ .config/bouncerrc.ts
@@ -3,13 +3,13 @@ export default {
     {
       name: `database.local.zthunworks.com`,
       paths: {
-        "/": "helpful-mongo-admin:8081",
+        "/": "bouncer-mongo-admin:8081",
       },
     },
     {
       name: `email.local.zthunworks.com`,
       paths: {
-        "/": "helpful-email",
+        "/": "bouncer-email",
       },
     },
   ],
