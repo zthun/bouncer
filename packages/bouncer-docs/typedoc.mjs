08667f6c747ce62730b37b92049c893bda95d8c5
Anthony Bonta <zthun.coding@gmail.com>
2026-01-05 11:09:54 -0600

    refactor: bouncer web is now bouncer docs (typescript)
    Bouncer web is in line to become the website to showcase the bouncer application

--- packages/bouncer-docs/typedoc.mjs
+++ packages/bouncer-docs/typedoc.mjs
@@ -3,5 +3,6 @@ import { ZTypedocConfigBuilder } from "@zthun/janitor-build-config/typedoc";
 export default new ZTypedocConfigBuilder()
   .web()
   .entry("../*")
+  .exclude("../bouncer-web")
   .favicon("public/images/svg/bouncer.svg")
   .build();
